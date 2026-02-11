dfd9b96e3ee41cd9ea93f1f16c0a29da88464eca
Chandini <chandini@Chandinis-MacBook-Air.local>
2026-02-10 22:28:33 -0600

    updated

--- module/ec2-module/outputs.tf
+++ module/ec2-module/outputs.tf
@@ -1,5 +1,5 @@
-output "public_ip_address"{
-    value = aws_instance.ec2_sample.public_ip
+output "public_ip_address" {
+  value = aws_instance.ec2_sample.public_ip
 }
 output "instance_id" {
   value = aws_instance.ec2_sample.id
--- module/global-tag/main.tf
+++ module/global-tag/main.tf
@@ -1,10 +1,10 @@
 locals {
-    tagging = {
-        environment = var.env
-        owner = var.owner
-        application = var.application
-        region = var.region
-        resourcetype = var.resourcetype
+  tagging = {
+    environment  = var.env
+    owner        = var.owner
+    application  = var.application
+    region       = var.region
+    resourcetype = var.resourcetype
 
-    }
+  }
 }
